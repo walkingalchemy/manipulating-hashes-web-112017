8d98999acbc371ba9c8c9e99dd11b8ca3a670a33
James Royer <walkingalchemy@gmail.com>
2017-10-30 18:59:08 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,8 +1,10 @@
+require 'pry'
+
 def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -14,9 +16,16 @@ def first_challenge
   }
 
   #your code here
-
+  contacts.map do |name, data|
+    if name == "Freddy Mercury"
+      data.map do |key, value|
+        if key == :favorite_icecream_flavors
+          value.delete_if {|val| val == "strawberry" }
+        end
+      end
+    end
+  end
 
   #remember to return your newly altered contacts hash!
   contacts
 end
-
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def second_challenge
   groceries = {
    dairy: ["milk", "yogurt", "cheese"],
@@ -7,7 +8,8 @@ def second_challenge
   }
 
   #code your solution here!
-
-  
+  new = []
+  groceries.values.each {|list| list.each {|val| new << val}}
+  new
 
 end
